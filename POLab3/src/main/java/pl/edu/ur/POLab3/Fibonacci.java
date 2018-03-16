da6f575f3276da07fcc9a0a10e70551a8e132c0d
Kamil Machański <>
2018-03-16 15:41:03 +0100

    fib

---
+++ POLab3/src/main/java/pl/edu/ur/POLab3/Fibonacci.java
@@ -0,0 +1,14 @@
+package pl.edu.ur.POLab3;
+
+public class Fibonacci {
+
+    public int fibReku(int n) {
+
+        if (n < 2) {
+            return n; 
+        } else {
+            return fibReku(n - 1) + fibReku(n - 2); 
+        }
+    }
+
+}
