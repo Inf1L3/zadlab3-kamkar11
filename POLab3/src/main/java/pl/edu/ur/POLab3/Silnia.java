a590ed7c2e148b4ed1a0e05bbc6cdc179033f5cf
Kamil Machański <>
2018-03-16 14:21:11 +0100

    silnia iteracyjnie

--- POLab3/src/main/java/pl/edu/ur/POLab3/Silnia.java
+++ POLab3/src/main/java/pl/edu/ur/POLab3/Silnia.java
@@ -1,38 +1,32 @@
 package pl.edu.ur.POLab3;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author student
- */
+
 public class Silnia {
-    // metoda silnia zwraca silnię z liczby przekazanej jako parametr
-// obliczenie silni odbywa się za pomocą rekurencji
+    
 
     public static int silnia(int wartosc) {
-// jeśli przekazany parametr jest równy zero to zwróć 1
-// a w przeciwnym wypadku zwróć wartość parametru * wywołanie metody silnia
-// z parametrem o jeden mniejszym
-//silnia reku
-/*
+
+        //silnia rekurencyjnie
+        
+         /*
         if (wartosc == 0) {
             return 1;
         } else {
             return wartosc * silnia(wartosc - 1);
         }
          */
-// silnia ite
-        if (wartość == 0) {
+         
+        // silnia iteracyjnie
+        if (wartosc == 0) {
             return 1;
         } else {
             int i = 1, s = 1;
-            while (i <= n) {
+            while (i <= wartosc) {
                 s = s * i;
                 i++;
             }
+            return s;
         }
-    }
+        
+    }   
+}
